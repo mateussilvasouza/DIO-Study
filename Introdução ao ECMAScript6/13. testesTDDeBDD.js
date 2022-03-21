c9cf4328ff9e38795f4f533c2eb89e35460318c2
Mateus Silva Souza <mathheussilvasouza@gmail.com>
2022-03-21 09:27:20 -0300

    Introdução a Testes, TDD e BDD

---
+++ Introdução ao ECMAScript6/13. testesTDDeBDD.js
@@ -0,0 +1,36 @@
+//Tipos de testes existentes
+
+/**Testes Automatizados:
+ * Testes uniários;
+ * Testes integrados;
+ * Testes funcionais.
+ */
+
+/**Testes manuais e automatizados:
+ * Testes usabilidade;
+ * Testes de aceitação do usuário;
+ * Protótipos;
+ * Testes funcionais;
+ * Exemplos;
+ * Alpha e Beta;
+ * Mais...
+ */
+
+/**Ferramentas de teste
+ * Teste de carga e performance;
+ * Teste de segurança;
+ * Mais...
+ */
+
+/**Tdd (Test Driven Development)
+ * É um dos pilares do Extreme programming, consiste em testar e
+ * refatorar em pequenos ciclos, onde você escreve o teste antes
+ * do código, faz o mesmo passar e refatora o código.
+ */
+
+/**Vantagens
+ * Feedback rápido;
+ * Maior segurança em alterações e novas funcionalidades;
+ * Código mais limpo;
+ * Produtividade.
+ */
