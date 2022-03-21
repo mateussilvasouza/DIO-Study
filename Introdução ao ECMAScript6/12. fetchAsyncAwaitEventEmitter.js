fec7b140157fc3fc694f070bff45510a591f2ee7
Mateus Silva Souza <mathheussilvasouza@gmail.com>
2022-03-21 09:01:02 -0300

    funcionamento do Async/Await

--- Introdução ao ECMAScript6/12. fetchAsyncAwaitEventEmitter.js
+++ Introdução ao ECMAScript6/12. fetchAsyncAwaitEventEmitter.js
@@ -1,7 +1,20 @@
+//fetch trabalha com promises
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+// .then(responseStream => {
+//     responseStream.json().then(data => {
+//         console.log(data);
+//     })
+// });
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-.then(responseStream => {
-    responseStream.json().then(data => {
-        console.log(data);
-    })
-});
+//Async / Await cria e trabalha com promises de maneira mais simples e enxuta
+
+const simpleFunc = async() => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => {
+        response.json().then(data => {
+            console.log(data);
+        });
+    });
+}
+
+simpleFunc();
