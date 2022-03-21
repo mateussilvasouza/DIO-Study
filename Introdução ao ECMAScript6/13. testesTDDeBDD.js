31321537ecd02a41711235f8cde61fcea1a0be80
Mateus Silva Souza <mathheussilvasouza@gmail.com>
2022-03-21 10:27:25 -0300

    Conceitos de testes, TDD e BDD

--- Introdução ao ECMAScript6/13. testesTDDeBDD.js
+++ Introdução ao ECMAScript6/13. testesTDDeBDD.js
@@ -22,15 +22,32 @@
  * Mais...
  */
 
-/**Tdd (Test Driven Development)
+/**TDD (Test Driven Development)
  * É um dos pilares do Extreme programming, consiste em testar e
  * refatorar em pequenos ciclos, onde você escreve o teste antes
  * do código, faz o mesmo passar e refatora o código.
- */
-
-/**Vantagens
+ * 
+ * Escrita do teste -> escrita do código -> refatoração
+ *
+ **Vantagens:
  * Feedback rápido;
  * Maior segurança em alterações e novas funcionalidades;
  * Código mais limpo;
  * Produtividade.
+ */
+
+/**BDD (Behavior Driven Development)
+ * 
+ * Técnica de Desenvolvimento Ágil que visa integrar regras de
+ * negócio com linguagens de programação.
+ * 
+ * *Pilares:
+ * Testes;
+ * Documentação;
+ * Exemplos.
+ * 
+ * *Vantagens:
+ * Compartilhamento de conhecimento;
+ * Documentação Dinâmica;
+ * Visão do todo.
  */
