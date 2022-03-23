a0af25a70c4cd0a8e4e6d11ed80d34554222ea1c
Mateus Silva Souza <mathheussilvasouza@gmail.com>
2022-03-23 08:05:22 -0300

    Entendendo o estado e ciclo de vida, inicialização, montagem e ciclo de vida.

--- Introdução ao ReactJS/README.md
+++ Introdução ao ReactJS/README.md
@@ -4,5 +4,5 @@
 * JSX (Permite a criação de componentes sem a a necessidade de utilizar React.createElement; o que torna a renderização e criação elementos de forma mais facil. O JSX não é interpretado pelo navegador, por isso necessitamos de um transpilador, nesse caso o Babel.);
 * Renderização;
 * Componentes e Props;
-* Estado e Ciclo de Vida;
-* Ecossistema do React;
+* Estado e Ciclo de Vida (Inicialiação, montagem, atualização e desmontagem.);
+* Ecossistema do React(Algumas ferramentas: React Router, Redux, Material UI, Ant-Design, Storybook, Gatsby; Jest; React i18n Next.);
--- Introdução ao ReactJS/src/index.js
+++ Introdução ao ReactJS/src/index.js
@@ -1,29 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import "./style.css";
 
 /**Importando componente */
 import Button from './Button';
 
-const sum = (a,b) => {
-    alert(a + b);
-}
+// const sum = (a,b) => {
+//     alert(a + b);
+// }
 
-const primeiroJSX = () => {
-    return(
-        <div className='custom'>
-            Mateus Silva Souza - Introdução ao ReactJS
-        </div>
-    );
-}
+// const primeiroJSX = () => {
+//     return(
+//         <div className='custom'>
+//             Mateus Silva Souza - Introdução ao ReactJS
+//         </div>
+//     );
+// }
 
-const App = () => {
-    return(
-        <div className='App'>
-            {primeiroJSX()}
-            <Button onClick={()=> sum(10,20)} name="Mateus Silva Souza"/>
-        </div>
-    );
+// const App = () => {
+//     return(
+//         <div className='App'>
+//             {primeiroJSX()}
+//             <Button onClick={()=> sum(10,20)} name="Mateus Silva Souza"/>
+//         </div>
+//     );
+// }
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            clock: 1000,
+            copo: 'Água',
+        }
+    }
+
+    componentDidMount(){
+        window.setTimeout(() => {
+            this.setState({
+                copo: 'Suco'
+            })
+        }, 3000);
+    }
+
+    alterarCopo = () =>{
+        this.setState({
+            copo:'Refrigerante'
+        });
+    }
+
+    render(){
+
+        const {clock} = this.state;
+        return(
+            <div>
+                <h1>{this.state.clock}</h1>
+                <Button onClick={() => this.alterarCopo()} name={this.state.copo}/>
+            </div>
+        )
+    }
 }
 
 const rootElement = document.getElementById("root");
